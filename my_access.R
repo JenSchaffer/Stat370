944b79eff1ea09874583409ed1923aae6a95d767
Jennifer Schaffer <Jennifer Schaffer>
2017-03-13 00:27:26 +0000

    Twitter Access Code

---
+++ my_access.R
@@ -0,0 +1,7 @@
+consumer_key <- "li4D1jzkNCYPFI6P0pCyE0YlF"
+consumer_secret <- "kqAKs6LMoarPtEDTzcPawfmZ3vclwDRcNMo1LbYrNHgJBb5crA"
+access_token <- "482413194-OnQqbSBUW9xsTkJv2HnYL8vefTqA1VOTyEYWGWHp"
+access_secret <- "FkOaGPCQp2NnOviDXDu4ha1Q30IeA9uWiR8j1Upjw1rO2"
+
+library(twitteR)
+setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
