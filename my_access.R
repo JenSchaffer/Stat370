6e2c9b465d02114fbaa179f1b8a4c8317fb6d54f
Jennifer Schaffer <Jennifer Schaffer>
2017-03-28 12:16:41 -0400

    Update

--- my_access.R
+++ my_access.R
@@ -1,7 +1,7 @@
-consumer_key <- "li4D1jzkNCYPFI6P0pCyE0YlF"
-consumer_secret <- "kqAKs6LMoarPtEDTzcPawfmZ3vclwDRcNMo1LbYrNHgJBb5crA"
-access_token <- "482413194-OnQqbSBUW9xsTkJv2HnYL8vefTqA1VOTyEYWGWHp"
-access_secret <- "FkOaGPCQp2NnOviDXDu4ha1Q30IeA9uWiR8j1Upjw1rO2"
+consumer_key <- "hh3tbRo5PpFpd0qEfmGXenSsS"
+consumer_secret <- "GVcQOuQ8eLzmxYfqyEU6dYvzzGFJPYKc2aR4Lp5JK195Xb7NSs"
+access_token <- "482413194-JfqqJe7Vh0MAnZnfraGfQ5BvcTOoIoF1Ocm0FGPQ"
+access_secret <- "HkE9exoVyqngXbaYe04912NEOPmPX6wHlm292D1GzsmMW"
 
 library(twitteR)
 setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
